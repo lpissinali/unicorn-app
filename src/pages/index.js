b823dae8a2facb649c1b1e5662dc4e1778883f2a
lpissinali <pissinali@gmail.com>
2023-04-17 15:13:26 +0200

    Populate first graph

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,52 @@
-import { Inter } from 'next/font/google';
 import MainLayout from '@/layouts/mainLayout';
-import {Avatar, Button, Card, Col, Row, Space} from 'antd';
+import {Avatar, Button, Card, Col, Image, Row, Space} from 'antd';
 import { AlignLeftOutlined, DownloadOutlined, FilterOutlined } from '@ant-design/icons';
 import mainStyle from '@/styles/main.module.css'
 import { useEffect, useState } from 'react';
 import { Chart } from 'chart.js';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [covidData, setCovidData] = useState([]);
 
   useEffect(() => {
     // Load token data
-    fetch('https://api.coronavirus.data.gov.uk/v1/data')
-      .then((res) => res.json())
-      .then((data) => {
-        setCovidData(data);
-        console.log(data);
-      });
+    fetch('https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=england&structure={"date":"date","newCases":"newCasesByPublishDate","newDeaths28DaysByDeathDate":"newDeaths28DaysByDeathDate"}')
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.data);
+          setCovidData(data.data.slice(0, 7).reverse());
+        });
   }, []);
+
+  useEffect(() => {
+    const ctx = document.getElementById('lineChart').getContext('2d');
+    const filteredData = [];
+    const myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: covidData.map(item => (
+            item.date
+        )),
+        datasets: [{
+          data: covidData.map(item => (
+              item.newDeaths28DaysByDeathDate
+          )),
+          label: "Daily Deaths",
+          backgroundColor: [
+            "rgb(75, 192, 192 )"
+          ],
+        }
+        ]
+      },
+    });
+  }, [covidData]);
 
   useEffect(() => {
     const ctx = document.getElementById('barChart').getContext('2d');
     const myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-        datasets: [{
-          data: [66, 144, 146, 116, 107, 131, 43],
-          label: "Applied",
-          borderColor: "rgb(109, 253, 181)",
-          backgroundColor: "rgb(109, 253, 181,0.5)",
-          borderWidth: 2
-        }, {
-          data: [40, 100, 44, 70, 63, 30, 10],
-          label: "Accepted",
-          borderColor: "rgb(75, 192, 192)",
-          backgroundColor: "rgb(75, 192, 192,0.5)",
-          borderWidth: 2
-        }, {
-          data: [20, 24, 50, 34, 33, 23, 12],
-          label: "Pending",
-          borderColor: "rgb(255, 205, 86)",
-          backgroundColor: "rgb(255, 205, 86,0.5)",
-          borderWidth: 2
-        }, {
-          data: [6, 20, 52, 12, 11, 78, 21],
-          label: "Rejected",
-          borderColor: "rgb(255, 99, 132)",
-          backgroundColor: "rgb(255, 99, 132,0.5)",
-          borderWidth: 2
-        }
-        ]
-      },
-    });
-  }, []);
-
-  useEffect(() => {
-    const ctx = document.getElementById('circleChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-        labels: ["Accepted", "Pending", "Rejected"],
+        labels: ["Unvaccinated", "1st dose", "2nd dose"],
         datasets: [{
           data: [70, 10, 6],
           borderColor: [
@@ -97,7 +81,7 @@ export default function Home() {
       <Space className={mainStyle.mainBody} direction='vertical' size={10} align='baseline'>
         <div className={mainStyle.headerSection}>
           <div>
-            <h2>England COVID stats</h2>
+            <h3>England COVID stats</h3>
           </div>
           <div className={mainStyle.toolbarSection}>
             <Button icon={<DownloadOutlined />}>
@@ -115,19 +99,19 @@ export default function Home() {
           <Col span={12}>
             <Card title="Daily death by COVID-19 in England"
               actions={[
-                <Avatar size={25} src={<img src="./avatar.webp" alt="avatar" />} />,
+                <Avatar key={'avatar'} size={25} src={<Image src="./avatar.webp" alt="avatar" />} />,
                 <AlignLeftOutlined key="notes" />,
               ]}>
-              <canvas id="barChart"></canvas>
+              <canvas id="lineChart"></canvas>
             </Card>
           </Col>
           <Col span={12}>
             <Card title="People tested positive in England"
               actions={[
-                <Avatar size={25} src={<img src="./avatar.webp" alt="avatar" />} />,
+                <Avatar key={'avatar'} size={25} src={<Image src="./avatar.webp" alt="avatar" />} />,
                 <AlignLeftOutlined key="notes" />,
               ]}>
-              <canvas id="circleChart"></canvas>
+              <canvas id="barChart"></canvas>
             </Card>
           </Col>
         </Row>
